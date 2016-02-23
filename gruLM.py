52284379f03214525af4d2ffd6beb0124e16b71e
snakeztc <zhaotiancheng.hz@gmail.com>
2016-02-23 00:33:16 -0500

    working own version

--- gruLM.py
+++ gruLM.py
@@ -8,7 +8,7 @@ from keras.preprocessing import sequence
 import numpy as np
 
 nb_word = 1000
-maxlen = 200  # cut texts after this number of words (among top max_features most common words)
+maxlen = 10  # cut texts after this number of words (among top max_features most common words)
 batch_size = 32
 
 print('Loading data...')
@@ -40,6 +40,7 @@ label_test = sequence.pad_sequences(label_test, maxlen=maxlen, padding='post', v
 print('X_train shape:', X_train.shape)
 print('X_test shape:', X_test.shape)
 
+
 # make index larger be 1-based
 X_train += 1
 X_test += 1
@@ -56,30 +57,49 @@ for i, s in enumerate(label_train):
         if w > 0:
             Y_train[i, t, w-1] = 1
 
-Y_test = np.zeros((label_test.shape[0], maxlen, nb_word), dtype=np.bool)
-for i, s in enumerate(label_test):
-    for t, w in enumerate(s):
-        if w > 0:
-            Y_test[i, t, w-1] = 1
-
 
 print('Build model...')
 model = Sequential()
-model.add(Embedding(nb_word, 300, input_length=maxlen, mask_zero=True))
-model.add(GRU(512, return_sequences=True))  # try using a GRU instead, for fun
+model.add(Embedding(nb_word, 200, input_length=maxlen, mask_zero=True))
+model.add(GRU(128, return_sequences=True))  # try using a GRU instead, for fun
 model.add(Dropout(0.2))
 model.add(TimeDistributedDense(nb_word))
 model.add(Activation('softmax'))
 
 # try using different optimizers and different optimizer configs
-model.compile(loss='categorical_crossentropy',
-              optimizer='adam')
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
 
 print("Train...")
-model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=20,
-          validation_data=(X_test, Y_test), show_accuracy=True)
-score, acc = model.evaluate(X_test, Y_test,
-                            batch_size=batch_size,
-                            show_accuracy=True)
-print('Test score:', score)
-print('Test accuracy:', acc)
+nb_epoch = 20
+num_samples = X_train.shape[0]
+cur_index = np.arange(num_samples)
+nb_batches = int(np.ceil(num_samples / float(batch_size)))
+
+
+def get_perplexity(m, X, label):
+    prob = m.predict_proba(X, verbose=False)
+    num_tokens = 0
+    sum_neg_prob = 0.0
+    for i, s in enumerate(label):
+        for t, w in enumerate(s):
+            sum_neg_prob += np.log2(prob[i, t, w-1])
+            num_tokens += 1
+    return pow(2, -1 * sum_neg_prob/num_tokens)
+
+
+for i_epoch in range(20):
+    print 'Epoch ' + str(i_epoch)
+    # shuffle data
+    np.random.shuffle(cur_index)
+    for iter_idx in range(nb_batches):
+        start_idx = iter_idx * batch_size
+        end_idx = np.min([(iter_idx+1) * batch_size, num_samples])
+        mini_batch_index = cur_index[start_idx:end_idx]
+        model.fit(X_train[mini_batch_index, :], Y_train[mini_batch_index, :, :], batch_size=batch_size,
+                  nb_epoch=1, verbose=False)
+
+    # calculate validation perplexity
+    print "Training perplexity is " + str(get_perplexity(model, X_train, label_train))
+    print  "Validation perplexity is " + str(get_perplexity(model, X_test, label_test))
+
+
